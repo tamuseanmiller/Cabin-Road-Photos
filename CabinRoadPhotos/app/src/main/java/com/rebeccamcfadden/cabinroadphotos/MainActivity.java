2506d351745fdfc2c344d6c7a7cdf5184bd0de11
Sean Miller <sean@millerfamily.tech>
2020-12-15 23:17:52 -0600

    Multiple UI Updates
    * Added GalleryView and Fullscreen ImageView for first 20 images.
    
    * Added Gallery button

--- CabinRoadPhotos/app/src/main/java/com/rebeccamcfadden/cabinroadphotos/MainActivity.java
+++ CabinRoadPhotos/app/src/main/java/com/rebeccamcfadden/cabinroadphotos/MainActivity.java
@@ -6,27 +6,37 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.google.android.material.button.MaterialButton;
 import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.UserCredentials;
 import com.google.photos.library.v1.PhotosLibraryClient;
 import com.google.photos.library.v1.PhotosLibrarySettings;
+import com.google.photos.types.proto.MediaItem;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.FormEncodingBuilder;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
+import com.stfalcon.imageviewer.StfalconImageViewer;
+import com.yanzhenjie.album.Album;
+import com.yanzhenjie.album.AlbumConfig;
+import com.yanzhenjie.album.AlbumFile;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class MainActivity extends AppCompatActivity {
 
     public static String accessToken;
     private static PhotosLibraryClient photosLibraryClient;
+    private Thread t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +45,74 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getAccessToken();
+
+        AtomicReference<StfalconImageViewer> stfalconImageViewer = new AtomicReference<>(null);
+
+        Thread t2 = null;
+
         // Photos access thread
-        Thread t1 = new Thread(() -> {
-            getAccessToken();
+        t1 = new Thread(() -> {
+
+
             getPhotosLibrary();
+//            Arrays.asList(photosLibraryClient.listMediaItems().getPage().getResponse().getMediaItems(0).getProductUrl().trim());
+            ArrayList<String> images = new ArrayList<>();
+            ArrayList<AlbumFile> albums = new ArrayList<>();
+            for (MediaItem image : photosLibraryClient.listMediaItems().getPage().getResponse().getMediaItemsList()) {
+                images.add(image.getBaseUrl().trim());
+                AlbumFile af = new AlbumFile();
+                af.setPath(image.getBaseUrl().trim());
+                albums.add(af);
+            }
+
+            Album.initialize(AlbumConfig.newBuilder(this)
+                    .setAlbumLoader(new MediaLoader())
+                    .build());
+
+            MaterialButton gallery = findViewById(R.id.gallery_button);
+            gallery.setOnClickListener(v -> {
+
+                runOnUiThread(() -> {
+
+                    Album.image(this)
+                            .singleChoice() // Multi-Mode, Single-Mode: singleChoice().
+                            .columnCount(4) // The number of columns in the page list.
+                            .onResult(result -> {
+                                ArrayList<String> results = new ArrayList<>();
+
+                                // Add images before and after selected image
+                                results.add(result.get(0).getPath());
+                                results.addAll(images);
+                                stfalconImageViewer.set(new StfalconImageViewer.Builder<>(this, results, (imageView, image) -> {
+                                    Glide.with(this).load(image).into(imageView);
+                                }).show());
+
+                            })
+                            .onCancel(result -> {
+                                // The user canceled the operation.
+                            })
+                            .start();
+                });
+            });
         });
-        t1.start();
+
+        t2 = new Thread(() -> {
+            int cnt = 0;
+            while (true) {
+                if (stfalconImageViewer.get() != null) {
+                    int finalCnt = cnt;
+                    runOnUiThread(() -> stfalconImageViewer.get().setCurrentPosition(finalCnt));
+                    cnt++;
+                }
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t2.start();
 
     }
 
@@ -79,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
                 .add("client_id", Properties.getWebAPIKey())   // something like : ...apps.googleusercontent.com
                 .add("client_secret", Properties.getWebSecretKey())
                 .add("redirect_uri", "")
-                .add("code", new SharedPreferencesManager(getApplicationContext()).retrieveString("code", "NULL")) // device code.
+                .add("code", new SharedPreferencesManager(getApplicationContext()).retrieveString("code", "NULL") /*LoginActivity.getAccount().getServerAuthCode()*/) // device code.
                 .add("id_token", LoginActivity.getIdToken()) // This is what we received in Step 5, the jwt token.
                 .build();
 
@@ -103,6 +175,8 @@ public class MainActivity extends AppCompatActivity {
                     final String message = jsonObject.toString(5);
                     accessToken = jsonObject.get("access_token").toString();
                     Log.i("Auth", message);
+
+                    t1.start();
 
                 } catch (JSONException e) {
                     e.printStackTrace();
