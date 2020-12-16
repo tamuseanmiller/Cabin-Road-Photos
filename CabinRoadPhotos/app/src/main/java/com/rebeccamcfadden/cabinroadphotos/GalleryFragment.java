77c63fb4f688374c11844e875084e44557d12b07
Rebecca McFadden <rmcfadden@tamu.edu>
2020-12-16 15:35:46 -0500

    settings page

--- CabinRoadPhotos/app/src/main/java/com/rebeccamcfadden/cabinroadphotos/GalleryFragment.java
+++ CabinRoadPhotos/app/src/main/java/com/rebeccamcfadden/cabinroadphotos/GalleryFragment.java
@@ -2,6 +2,7 @@ package com.rebeccamcfadden.cabinroadphotos;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
