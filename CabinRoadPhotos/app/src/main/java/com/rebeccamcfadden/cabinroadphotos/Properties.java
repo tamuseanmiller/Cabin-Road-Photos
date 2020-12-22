42d7735f238c4fd871dc979dbc6127d73775e6a0
Sean Miller <42820988+tamuseanmiller@users.noreply.github.com>
2020-12-21 19:54:38 -0600

    Update Properties.java

--- CabinRoadPhotos/app/src/main/java/com/rebeccamcfadden/cabinroadphotos/Properties.java
+++ CabinRoadPhotos/app/src/main/java/com/rebeccamcfadden/cabinroadphotos/Properties.java
@@ -3,10 +3,10 @@ package com.rebeccamcfadden.cabinroadphotos;
 public class Properties {
 
     public static String getWebAPIKey() {
-        return "806882709826-07971357nbqs6j314o72bpbak80l3mb1.apps.googleusercontent.com";
+        return "";
     }
 
     public static String getWebSecretKey() {
-        return "BS_PX2HsqWTOzWv_07SgI7tF";
+        return "";
     }
 }
